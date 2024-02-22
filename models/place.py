a2a4ecc8edd6d0a0c7fc6d62baeee3128aa84ae8
TONNY BAWEMBYE <tonnybaw75@gmail.com>
2024-02-22 10:17:47 +0300

    review objects

--- models/place.py
+++ models/place.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.review import Review
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer(), nullable=False, default=0)
+    number_bathrooms = Column(Integer(), nullable=False, default=0)
+    max_guest = Column(Integer(), nullable=False, default=0)
+    price_by_night = Column(Integer(), nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    # amenity_ids = []
+
+    reviews = relationship('Review', backref='place',
+                           cascade="save-update, delete")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        from models import storage
+
+        @property
+        def reviews(self):
+            reviews_list = []
+            reviews_objs = storage.all(Review)
+            for k, v in reviews_objs.values():
+                if v.place_id == self.id:
+                    reviews_list.append(v)
+            return reviews_list
